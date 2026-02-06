cb26e9f07700be6e6f3dc7000dbd8ae6e12c8aaa
Loma <lomakim@gmail.com>
2026-02-05 19:55:28 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// COMSC210 | Lab 4A | Loma Kim
+// This program will create and store a vector of structs
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
