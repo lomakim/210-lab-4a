335ad429713b2af2fca02b3293707450180cc65c
Loma <lomakim@gmail.com>
2026-02-05 20:06:16 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,23 @@
 #include <vector>
 using namespace std;
 
+// STRUCT DECLARATION
+struct Color {
+    int red, green, blue;
+};
+
 int main(){
-    
+    // DECLARATIONS
+    vector <Color> vec;
+
+    // Test struct and empty vec
+    Color c = {10, 190, 75};
+
+    if (vec.empty()) {
+        cout << "Vector is empty.\n";
+    }
+
+    cout << c.red << ", " << c.green << ", " << c.blue;
+
     return 0;
 }
